b6e04758d075fec64a5c7890da5a43717ef98ff4
Lauren <laurencaroleen@gmail.com>
2022-01-14 05:04:43 +0000

    Completed 3 out of 4

---
+++ crud.py
@@ -0,0 +1,50 @@
+"""CRUD operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+# Functions start here!
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview=overview, release_date=release_date, poster_path=poster_path)
+
+    return movie
+
+def create_rating(user, movie, score):
+    """Create and return a new rating."""
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+    return rating
+
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    '''get movie by id'''
+
+    return Movie.query.get(movie_id)
+
+def get_users():
+    '''Return all users'''
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    '''get user by id'''
+
+    return User.query.get(user_id)
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
---
+++ server.py
@@ -0,0 +1,57 @@
+"""Server for movie ratings app."""
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db
+from jinja2 import StrictUndefined
+import crud
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def homepage():
+    """View homepage."""
+
+    return render_template('homepage.html')
+
+@app.route('/movies')
+def all_movies():
+    """View movies page."""
+
+    movies = crud.get_movies()
+
+    return render_template('all_movies.html', movies=movies)
+
+
+@app.route('/users')
+def all_users():
+    """View users page."""
+
+    users = crud.get_users()
+
+    return render_template('all_users.html', users=users)
+
+
+@app.route('/movies/<movie_id>')
+def show_movie(movie_id):
+    """Show movie detailes."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+    """Show user detailes."""
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_details.html', user=user)
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
